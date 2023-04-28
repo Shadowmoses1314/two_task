36040de921630818fb6dc7cb456853ddb042d506
Григорий Шабалкин <ShadowMoses1314@mail.ru>
2023-04-29 01:16:48 +0600

    Second

--- find_expression.py
+++ find_expression.py
@@ -1,17 +1,25 @@
 from itertools import product
 
 
-def find_expression(n, m):
+def find_expression(n: int, m: int) -> str | None:
     nums = [str(i) for i in range(1, n+1)]
     result = [num for num in ''.join(nums) if num != '0']
     count = len(result)
-    zero_idx = ''.join(nums).find('0')
+    result10 = result[:9] + ['10'] + result[10:]
+    for p in product(['', '+'], repeat=count-1):
+        expr = ''
+        for i in range(count-1):
+            expr += result10[i] + p[i]
+        expr += result10[count-1]
+        if eval(expr) == m:
+            return expr
     for p in product(['', '+'], repeat=count-1):
         expr = ''
         for i in range(count-1):
             expr += result[i] + p[i]
         expr += result[count-1]
         if eval(expr) == m:
+            zero_idx = ''.join(nums).find('0')
             second_one_idx = expr.find('1', 1)
             num_plus = expr[:second_one_idx].count('+')
             new_zero_idx = zero_idx + num_plus + 1
